b7a64d0a25a2c564c78fa0a488ad45122cef97a9
Tiago <tiagoguimaraes95@gmail.com>
2014-08-01 19:16:54 -0300

    COMMIT DATA 01/08

--- src/main/java/br/gsfarma/web/ProdutoBean.java
+++ src/main/java/br/gsfarma/web/ProdutoBean.java
@@ -3,8 +3,13 @@ package br.gsfarma.web;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 import br.gsfarma.produto.Produto;
 import br.gsfarma.produto.ProdutoRN;
@@ -19,6 +24,7 @@ public class ProdutoBean implements Serializable{
 	private static final long serialVersionUID = 115612075060736726L;
 	private Produto produto = new Produto();
 	private List<Produto> lista;
+	private UploadedFile arquivo;
 	
 	
 	public List<Produto> getLista(){
@@ -33,6 +39,25 @@ public class ProdutoBean implements Serializable{
 		return this.lista;
 	}
 
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+	
+	public String salvar(){
+		
+		if(arquivo != null){
+			FacesContext context = FacesContext.getCurrentInstance();
+			FacesMessage facesMessage = new FacesMessage("O arquivo: "+arquivo.getFileName()+" está aqui!!");
+			context.addMessage(null, facesMessage);
+		}else{
+			FacesContext context = FacesContext.getCurrentInstance();
+			FacesMessage facesMessage = new FacesMessage("O arquivo não está aqui!!");
+			context.addMessage(null, facesMessage);
+		}
+		
+		return null;
+	}
 
 	public Produto getProduto() {
 		return produto;
@@ -53,16 +78,23 @@ public class ProdutoBean implements Serializable{
 		this.lista = lista;
 	}
 
+	public UploadedFile getArquivo() {
+		return arquivo;
+	}
+
+	public void setArquivo(UploadedFile arquivo) {
+		this.arquivo = arquivo;
+	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((arquivo == null) ? 0 : arquivo.hashCode());
 		result = prime * result + ((lista == null) ? 0 : lista.hashCode());
 		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -73,6 +105,11 @@ public class ProdutoBean implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		ProdutoBean other = (ProdutoBean) obj;
+		if (arquivo == null) {
+			if (other.arquivo != null)
+				return false;
+		} else if (!arquivo.equals(other.arquivo))
+			return false;
 		if (lista == null) {
 			if (other.lista != null)
 				return false;
